3a9f596a614581adb911b9ec228e4e96088ba5b5
hasanali112 <mdhasan.ru.ro@gmail.com>
2023-05-18 20:19:39 +0600

    setup react router and install css framework page setup

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Pages/Shared/Header';
+import Footer from '../Pages/Shared/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
